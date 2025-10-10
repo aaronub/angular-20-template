2bf06bf3b087c005de1ee63e48accc42d5402d0f
Aaron <aaron.wang@wexinc.com>
2025-10-09 23:31:56 -0400

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('my-angular20-app');
+  
+  onButtonClick(type: string) {
+    console.log(`${type} button clicked!`);
+  }
 }
