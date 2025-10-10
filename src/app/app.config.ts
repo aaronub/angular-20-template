296bb7c1be69298cc3412dd4aba046ad559afa08
Aaron <aaron.wang@wexinc.com>
2025-10-10 10:20:19 -0400

    added wex-brand-theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,27 +3,28 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { definePreset } from '@primeuix/themes';
-import Aura from '@primeuix/themes/aura';
+// import Aura from '@primeuix/themes/aura';
+import WEXBrandTheme from '../../packages/wex-brand-theme/index';
 
 import { routes } from './app.routes';
 
-const IndigoPreset = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '{indigo.50}',
-      100: '{indigo.100}',
-      200: '{indigo.200}',
-      300: '{indigo.300}',
-      400: '{indigo.400}',
-      500: '{indigo.500}',
-      600: '{indigo.600}',
-      700: '{indigo.700}',
-      800: '{indigo.800}',
-      900: '{indigo.900}',
-      950: '{indigo.950}'
-    }
-  }
-});
+// const IndigoPreset = definePreset(Aura, {
+//   semantic: {
+//     primary: {
+//       50: '{indigo.50}',
+//       100: '{indigo.100}',
+//       200: '{indigo.200}',
+//       300: '{indigo.300}',
+//       400: '{indigo.400}',
+//       500: '{indigo.500}',
+//       600: '{indigo.600}',
+//       700: '{indigo.700}',
+//       800: '{indigo.800}',
+//       900: '{indigo.900}',
+//       950: '{indigo.950}'
+//     }
+//   }
+// });
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -33,7 +34,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: IndigoPreset,
+        preset: WEXBrandTheme,
         options: {
           cssLayer: {
             name: 'primeng',
